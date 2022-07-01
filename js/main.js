4c9323106c0bf48a6338024b86711b8e95c980c8
chanmin0905 <jcm0905@gmail.com>
2022-07-02 03:36:33 +0900

    07.Header-and-Dropdownimenu:Search

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-console.log("Hello");
+const searchEl = document.querySelector(".search");
+const searchInputEl = searchEl.querySelector("input");
+
+console.log(searchEl);
+console.log(searchInputEl);
+
+searchEl.addEventListener("click", () => {
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener("focus", () => {
+  searchEl.classList.add("focused");
+  searchInputEl.setAttribute("placeholder", "통합검색");
+});
+
+searchInputEl.addEventListener("blur", () => {
+  searchEl.classList.remove("focused");
+  searchInputEl.setAttribute("placeholder", "");
+});
